f8e63d5fc750283159a657ecb9e193cefee92bdf
RJ <rjyansonx@gmail.com>
2023-09-14 13:41:32 +0800

    Add file

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 'use client'
 import Banner from '@components/Landing/banner'
+import AboutUs from '@components/Landing/aboutUs'
 
 export default function Home() {
 	return (
 		<main className="flex bg-white min-h-screen flex-col items-center justify-between">
 			<div className="w-full h-[200vh] float-left px-[200px]">
 				<Banner />
+				<AboutUs />
 			</div>
 		</main>
 	)
