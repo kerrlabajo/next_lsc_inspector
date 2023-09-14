038ec840550eca4dd7ebe8252e1aaacaeb7cec75
RJ <rjyansonx@gmail.com>
2023-09-14 14:15:20 +0800

    Add feature component

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 'use client'
 import Banner from '@components/Landing/banner'
 import AboutUs from '@components/Landing/aboutUs'
+import Features from '@components/Landing/features'
 
 export default function Home() {
 	return (
 		<main className="flex bg-white min-h-screen flex-col items-center justify-between">
-			<div className="w-full h-[200vh] float-left px-[200px]">
+			<div className="w-full h-[300vh] float-left px-[200px]">
 				<Banner />
+				<Features />
 				<AboutUs />
 			</div>
 		</main>
---
+++ src/components/Landing/features.js
@@ -0,0 +1,61 @@
+import Image from 'next/image'
+import React from 'react'
+import Feature1 from '../../../public/assets/images/feature1.svg'
+import Feature2 from '../../../public/assets/images/Feature2.svg'
+import Feature3 from '../../../public/assets/images/Feature3.svg'
+
+const Features = () => {
+	return (
+		<div className="bg-white h-[100vh] float-left text-neutral-900 w-full flex items-center justify-center">
+			<div className="w-full flex items-center flex-col gap-y-[80px]">
+				<div className="flex flex-col items-center justify-center">
+					<h1 className="text-5xl font-bold text-green-500">Features</h1>
+					<span className="w-[45%] text-center mt-[10px]">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod mauris at dui rhoncus, vel accumsan mi vestibulum. Praesent non
+						placerat sapien, sed maximus risus.
+					</span>
+				</div>
+				<div className="flex">
+					<div className="flex items-center justify-center flex-col border-r-[1px] border-neutral-700 gap-y-[15px] p-[40px]">
+						<Image
+							src={Feature1}
+							alt="feature1"
+							width={50}
+						/>
+						<h1 className="text-[20px] font-bold">Feature 1</h1>
+						<span className="text-center">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod mauris at dui rhoncus, vel accumsan mi vestibulum. Praesent non
+							placerat sapien, sed maximus risus.
+						</span>
+					</div>
+					<div className="flex items-center justify-center flex-col border-r-[1px] border-neutral-700 gap-y-[15px] p-[40px]">
+						<Image
+							src={Feature2}
+							alt="feature2"
+							width={50}
+						/>
+						<h1 className="text-[20px] font-bold">Feature 2</h1>
+						<span className="text-center">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod mauris at dui rhoncus, vel accumsan mi vestibulum. Praesent non
+							placerat sapien, sed maximus risus.
+						</span>
+					</div>
+					<div className="flex items-center justify-center flex-col gap-y-[15px] p-[40px]">
+						<Image
+							src={Feature3}
+							alt="feature3"
+							width={50}
+						/>
+						<h1 className="text-[20px] font-bold">Feature 3</h1>
+						<span className="text-center">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod mauris at dui rhoncus, vel accumsan mi vestibulum. Praesent non
+							placerat sapien, sed maximus risus.
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Features
