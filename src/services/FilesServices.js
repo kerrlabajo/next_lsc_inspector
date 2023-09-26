abce95662176cee898f71f64d8ccc22f9fb05c03
Jhonray Acojedo <jhonray.acojedo@cit.edu>
2023-09-26 13:32:04 +0800

    Add Files Services

--- src/services/FilesServices.js
+++ src/services/FilesServices.js
@@ -3,10 +3,11 @@ import config from './config'
 
 const BASE_URL = `${config.API_URL}/files`
 const FilesServices = {
-    upload: (file) => axios.post(`${BASE_URL}/upload`, file),
+    upload: (file, headers) => axios.post(`${BASE_URL}/upload`, file, { headers }),
 	analyze: (file) => axios.post(`${BASE_URL}/analyze`, file),
     getAll: () => axios.get(`${BASE_URL}`),
     getById: (id) => axios.get(`${BASE_URL}/${id}`),
+    demoAnalyze: () => axios.get(`${BASE_URL}/${id}`)
 }
 
 export default FilesServices
