c5e36b430b10ab2d94998628628a75f1f830224e
Jhonray Acojedo <jhonray.acojedo@cit.edu>
2023-09-28 00:54:53 +0800

    implement analyze image

--- src/app/main/page.js
+++ src/app/main/page.js
@@ -9,18 +9,18 @@ import Toggle from '@components/Button/toggle'
 import useUserStore from '../../useStore'
 import useUpload from '@hooks/useUpload'
 import axios from 'axios'
+import useAnalyze from '@hooks/useAnalyze';
 
 const Main = () => {
 	const [isModalOpen, setIsModalOpen] = useState(true)
 	const { user, isAuthenticated } = useUserStore()
 	const { uploadFile } = useUpload()
+  const { analyzeFile } = useAnalyze()
 	const [file, setFile] = useState(null)
 	const [uploadedImageUrl, setUploadedImageUrl] = useState(null)
-	const headers = {
-        'Authorization' : user.user.access_token,
-		'content-type': 'multipart/form-data'
-        // Add other headers if needed
-    };
+  const [analyzedImage, setAnalyzedImage] = useState(null)
+  const [selectedModel, setSelectedModel] = useState('General')
+	const authorization =  user.user.access_token
 	const handleFileUpload = async () => {
 		if(file){
 			const formData = new FormData()
@@ -28,7 +28,7 @@ const Main = () => {
 
 			const response = await uploadFile({
 				body: formData,
-				headers: headers
+				headers: authorization
 			});
 			if (response) {
 				console.log('Image uploaded successfully')
@@ -39,16 +39,45 @@ const Main = () => {
 		}else{
 			console.error('No file selected')
 		}
-
 	};
 
+  const handleSelectModel = (modelName) => {
+    // Add your logic here to handle the selected model
+    setSelectedModel(modelName);
+    setIsModalOpen(false)
+  }
+  const handleAnalyze = async () => {
+    if (uploadedImageUrl) {
+        try {
+            const response = await analyzeFile({
+                fileUrl: uploadedImageUrl
+            }, authorization)
+
+            if (response.status === 201) {
+                console.log('Image analyzed successfully');
+                setAnalyzedImage(response.data); // Assuming response.data contains the analyzed data
+            } else {
+                console.error('Image analyze unsuccessful');
+            }
+        } catch (error) {
+            console.error('Error analyzing file:', error.message);
+        }
+    } else {
+        console.error('No file selected');
+    }
+  }
+
 	useEffect(() => {
-		// This block of code will run whenever uploadedImageUrl changes.
-		// It will log the updated URL to the console.
-		if (uploadedImageUrl) {
-		  console.log('Uploaded Image URL:', uploadedImageUrl);
-		}
-	  }, [uploadedImageUrl]);
+    // This block of code will run whenever either uploadedImageUrl or analyzedImage changes.
+    // It will log the updated URLs to the console.
+    if (uploadedImageUrl) {
+        console.log('Uploaded Image URL:', uploadedImageUrl);
+    }
+
+    if (analyzedImage) {
+        console.log('Analyzed Image URL:', analyzedImage);
+    }
+}, [uploadedImageUrl, analyzedImage]);
 
 	//console.log(headers.authorization)
 	const renderContent = () => {
@@ -62,10 +91,10 @@ const Main = () => {
 					<span className="text-gray-400">Select from any from our pre-defined model you want to use.</span>
 					<ul className="flex py-[20px] gap-3">
 						<li className="w-fit text-gray-500 px-[10px] py-[5px] cursor-pointer rounded-md border border-gray-300 hover:border-green-300 hover:text-green-500">
-							<span className="">General</span>
+							<span className="" onClick={handleSelectModel("General")}>General</span>
 						</li>
 						<li className="w-fit text-gray-500 px-[10px] py-[5px] cursor-pointer rounded-md border border-gray-300 hover:border-green-300 hover:text-green-500">
-							<span className="">Laser Solder</span>
+							<span className="" onClick={handleSelectModel("LaserSolder")}>Laser Solder</span>
 						</li>
 					</ul>
 				</div>
@@ -139,9 +168,45 @@ const Main = () => {
                 </div>
               </div>
               <Button
-                title="Upload"
+                title="Analyze"
                 style=" bg-green-400 text-white hover:bg-green-500 ml-auto"
+                onClick={ handleAnalyze}
               />
+            </div>
+          )}
+          {analyzedImage && (
+            <div className="w-full flex flex-row gap-x-36 mb-4 rounded shadow p-6">
+              <img
+                className="flex-shrink-0 w-1/3 h-1/2 object-cover"
+                src={analyzedImage.url}
+                alt="Uploaded"
+              />
+              <div className="flex flex-col flex-grow items-start">
+                <div className="flex items-center mb-2">
+                  <p className="font-bold mr-4" style={{ width: "100px" }}>
+                    Filename:
+                  </p>
+                  <p>test</p>
+                </div>
+                <div className="flex items-center mb-2">
+                  <p className="font-bold mr-4" style={{ width: "100px" }}>
+                    Dimensions:
+                  </p>
+                  <p>test</p>
+                </div>
+                <div className="flex items-center mb-2">
+                  <p className="font-bold mr-4" style={{ width: "100px" }}>
+                    Size:
+                  </p>
+                  <p>test</p>
+                </div>
+                <div className="flex items-center">
+                  <p className="font-bold mr-4" style={{ width: "100px" }}>
+                    Extension:
+                  </p>
+                  <p>test</p>
+                </div>
+              </div>
             </div>
           )}
         </div>
