e8df0832188fac07dee1a59c66400854d05afa24
RJ <rjyansonx@gmail.com>
2023-09-28 16:55:19 +0800

    uPDATE

--- src/hooks/useDemoAnalyze.js
+++ src/hooks/useDemoAnalyze.js
@@ -1,70 +1,26 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import FilesServices from '@services/FilesServices'
 
 const useDemoAnalyze = () => {
-    const [analyzing, setAnalyzing] = useState(false);
+	const [analyzing, setAnalyzing] = useState(false)
 
-    const analyze = async (file) => {
-        try {
-            setAnalyzing(true);
-            const response = await FilesServices.demoAnalyze(file);
-            if (response.status === 200){
-                return response.data
-            } else {
-                throw new Error(`Error analyzing file: ${response.statusText}`);
-            }
-        } catch (error) {
-            throw new Error(`Error analyzing file: ${error.message}`);
-        } finally {
-            setAnalyzing(false);
-        }
-    };
+	const analyze = async (file) => {
+		try {
+			setAnalyzing(true)
+			const response = await FilesServices.demoAnalyze(file)
+			if (response.status === 200) {
+				return response.data
+			} else {
+				throw new Error(`Error analyzing file: ${response.statusText}`)
+			}
+		} catch (error) {
+			throw new Error(`Error analyzing file: ${error.message}`)
+		} finally {
+			setAnalyzing(false)
+		}
+	}
 
-    return { uploadFile: analyze, uploading: analyzing }
+	return { uploadFile: analyze, uploading: analyzing }
 }
 
 export default useDemoAnalyze
-=======
-'use client'
-
-import { useState } from 'react'
-import FilesServices from '@services/FilesServices'
-
-const useAnalyze = () => {
-	const [isAnalyzing, setIsAnalyzing] = useState(false)
-
-	const analyzeFile = async ({ url }) => {
-		setIsAnalyzing(true)
-
-		let responseCode
-		let fileAnalyzed
-
-		try {
-			const { status, data } = await FilesServices.demoAnalyze({ url })
-
-			responseCode = status
-			fileAnalyzed = data
-			return { data, status }
-		} catch (error) {
-			responseCode = error.response.status
-		}
-
-		switch (responseCode) {
-			case 201:
-				break
-			case 401:
-				await callback.invalidFields()
-				break
-			case 500:
-				await callback.internalError()
-				break
-		}
-
-		setIsAnalyzing(false)
-	}
-	return { isAnalyzing, analyzeFile }
-}
-
-export default useAnalyze
->>>>>>> 29f1f22 (Implement analyze in demo page)
--- src/hooks/useUpload.js
+++ src/hooks/useUpload.js
@@ -1,41 +1,20 @@
+'use client'
+
 import { useState } from 'react'
 import FilesServices from '@services/FilesServices'
-import useUserStore from './../useStore'
 
-	const useUpload = () => {
-		const [isUploading, setIsUploading] = useState(false)
+const useUpload = () => {
+	const [isUploading, setIsUploading] = useState(false)
 
-		const uploadFile = async ({ body, headers }) => {
-			setIsUploading(true)
+	const uploadFile = async ({ body }) => {
+		setIsUploading(true)
 
-			let responseCode
-			let fileUploaded
-			console.log(body, headers)
-			try {
-				const { status, data } = await FilesServices.upload(body, headers)
+		let responseCode
+		let fileUploaded
 
-				responseCode = status
-				fileUploaded = data
-				return(fileUploaded)
-			} catch (error) {
-				//responseCode = error.response.status
-				console.log('iM HERE: ', body)
-			}
+		try {
+			const { status, data } = await FilesServices.upload(body)
 
-<<<<<<< HEAD
-			switch (responseCode) {
-				case 201:
-					break
-				case 401:
-					await callback.invalidFields()
-					break
-				case 500:
-					await callback.internalError()
-					break
-			}
-
-			setIsUploading(false)
-=======
 			responseCode = status
 			fileUploaded = data
 
@@ -43,9 +22,22 @@ import useUserStore from './../useStore'
 		} catch (error) {
 			responseCode = error.response.status
 			console.log('iM HERE: ', body)
->>>>>>> 29f1f22 (Implement analyze in demo page)
 		}
-		return { isUploading, uploadFile }
+
+		switch (responseCode) {
+			case 201:
+				break
+			case 401:
+				await callback.invalidFields()
+				break
+			case 500:
+				await callback.internalError()
+				break
+		}
+
+		setIsUploading(false)
 	}
+	return { isUploading, uploadFile }
+}
 
 export default useUpload
