aff2f4cb7bf3cb02a7a2b78db76a82901847db9f
RJ <rjyansonx@gmail.com>
2023-09-28 16:59:13 +0800

    update

--- src/hooks/useDemoAnalyze.js
+++ src/hooks/useDemoAnalyze.js
@@ -1,26 +1,41 @@
+'use client'
+
 import { useState } from 'react'
 import FilesServices from '@services/FilesServices'
 
-const useDemoAnalyze = () => {
-	const [analyzing, setAnalyzing] = useState(false)
+const useAnalyze = () => {
+	const [isAnalyzing, setIsAnalyzing] = useState(false)
 
-	const analyze = async (file) => {
+	const analyzeFile = async ({ url }) => {
+		setIsAnalyzing(true)
+
+		let responseCode
+		let fileAnalyzed
+
 		try {
-			setAnalyzing(true)
-			const response = await FilesServices.demoAnalyze(file)
-			if (response.status === 200) {
-				return response.data
-			} else {
-				throw new Error(`Error analyzing file: ${response.statusText}`)
-			}
-		} catch (error) {
-			throw new Error(`Error analyzing file: ${error.message}`)
-		} finally {
-			setAnalyzing(false)
-		}
-	}
+			const { status, data } = await FilesServices.demoAnalyze({ url })
 
-	return { uploadFile: analyze, uploading: analyzing }
+			responseCode = status
+			fileAnalyzed = data
+			return { data, status }
+		} catch (error) {
+			responseCode = error.response.status
+		}
+
+		switch (responseCode) {
+			case 201:
+				break
+			case 401:
+				await callback.invalidFields()
+				break
+			case 500:
+				await callback.internalError()
+				break
+		}
+
+		setIsAnalyzing(false)
+	}
+	return { isAnalyzing, analyzeFile }
 }
 
-export default useDemoAnalyze
+export default useAnalyze
