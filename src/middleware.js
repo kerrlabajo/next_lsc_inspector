de2313ce3ae15f09b7ec80cdad3f9e60934ed312
RJ <rjyansonx@gmail.com>
2023-10-20 01:51:39 +0800

    update

--- src/middleware.js
+++ src/middleware.js
@@ -3,9 +3,8 @@ import { NextResponse } from 'next/server'
 export function middleware() {
 	const res = NextResponse.next()
 
-	// add the CORS headers to the response
 	res.headers.append('Access-Control-Allow-Credentials', 'true')
-	res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+	res.headers.append('Access-Control-Allow-Origin', 'https://next-lsc-inspector.vercel.app')
 	res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
 	res.headers.append(
 		'Access-Control-Allow-Headers',
