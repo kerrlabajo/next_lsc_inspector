d76ebc686106a52fdab2c306e98a8c38b4cc333b
RJ <rjyansonx@gmail.com>
2023-10-20 04:01:18 +0800

    update

--- src/app/docs/page.js
+++ src/app/docs/page.js
@@ -7,9 +7,9 @@ import Link from 'next/link'
 export default function Docs() {
 	const [active, setActive] = useState('Getting Started')
 	return (
-		<Container style=" text-neutral-900 mt-[100px] flex justify-center">
-			<div className="w-[80%] flex gap-x-1 items-left justify-between mb-44 p-4">
-				<div className="w-[20%] h-full flex flex-col gap-4 text-sm sticky">
+		<Container style=" text-neutral-900 mt-[100px] flex justify-center relative">
+			<div className="w-[80%] flex gap-x-1 items-left justify-between mb-44 p-4 relative">
+				<div className="w-[20%] h-full flex flex-col gap-4 text-sm sticky top-0">
 					<div className="flex flex-col gap-2">
 						<h1 className="font-bold">Introduction</h1>
 						<div className="">
@@ -131,7 +131,7 @@ export default function Docs() {
 							<span className={``}>
 								To start building a computer vision model, you first need to create a Project. A Project contains all of your data, dataset versions,
 								and models for a given task. First, go to the Roboflow dashboard. Then, click{' '}
-								<span className="bg-gray-100 rounded text-sm p-1">&apos;Create New Project&apos;</span>:
+								<span className="bg-gray-100 rounded text-sm p-1 font-medium">Create New Project</span>:
 							</span>
 							<span className="my-6"> You will be asked to specify:</span>
 							<ol className="list-decimal pl-10">
@@ -174,7 +174,10 @@ export default function Docs() {
 								To upload data, first create a project if you do not have one already. When you first create a project, you will be asked to upload
 								images:
 							</span>
-							<span className="mt-6">If you already have a project, click &apos;Upload&apos; in the project sidebar to upload images.</span>
+							<span className="mt-6">
+								If you already have a project, click <span className="bg-gray-100  font-medium rounded text-sm p-1">Upload</span> in the project
+								sidebar to upload images.
+							</span>
 						</div>
 					</div>
 					<div className="flex flex-col gap-2">
@@ -192,14 +195,15 @@ export default function Docs() {
 						<h1 className="text-xl font-bold">Create a Dataset Version</h1>
 						<div className=" border-gray-700 flex flex-col">
 							<span className={``}>
-								To create a dataset version, click &apos;Versions&apos; in the sidebar associated with your Roboflow project. Then, click
-								&apos;Generate New Version&apos;. From this page, you can set a train/test/valid split and specify preprocessing steps and
-								augmentations for your new dataset version.
+								To create a dataset version, click <span className="bg-gray-100  font-medium rounded text-sm p-1">Versions</span> in the sidebar
+								associated with your Roboflow project. Then, click{' '}
+								<span className="bg-gray-100  font-medium rounded text-sm p-1">Generate New Version</span>. From this page, you can set a
+								train/test/valid split and specify preprocessing steps and augmentations for your new dataset version.
 							</span>
 							<span className="mt-4">
-								Once you have specified the preprocessing steps and augmentations you want to apply to your data, click &apos;Generate&apos;. This
-								will generate a new dataset version. You can then use this dataset version to train a model in Roboflow. You can also export your
-								dataset for use in training a model manually.
+								Once you have specified the preprocessing steps and augmentations you want to apply to your data, click{' '}
+								<span className="bg-gray-100  font-medium rounded text-sm p-1">Generate</span>. This will generate a new dataset version. You can then
+								use this dataset version to train a model in Roboflow. You can also export your dataset for use in training a model manually.
 							</span>
 						</div>
 					</div>
@@ -207,12 +211,13 @@ export default function Docs() {
 						<h1 className="text-xl font-bold">Export Dataset Version</h1>
 						<div className=" border-gray-700 flex flex-col">
 							<span className={``}>
-								To access this dataset, click &apos;Versions&apos; in the sidebar associated with your Roboflow project. Then select the dataset
-								version that you want to export and click &apos;Export Dataset&apos;.
+								To access this dataset, click <span className="bg-gray-100  font-medium rounded text-sm p-1">Versions</span> in the sidebar associated
+								with your Roboflow project. Then select the dataset version that you want to export and click Export Dataset.
 							</span>
 							<span className="mt-4">
-								From this page, you can select which format this dataset will be used and select &apos;show download code&apos; to get your API key,
-								and other details, otherwise, you can also download the dataset via zip file.
+								From this page, you can select which format this dataset will be used and select{' '}
+								<span className="bg-gray-100  font-medium rounded text-sm p-1">show download code</span> to get your API key, and other details,
+								otherwise, you can also download the dataset via zip file.
 							</span>
 						</div>
 					</div>
@@ -231,8 +236,8 @@ export default function Docs() {
 							<span className={``}>Represented by a hand icon, this feature allows you to select, edit, and drag individual annotations.</span>
 							<ul className="list-disc pl-10">
 								<li>
-									<b>Single-click</b> an existing bounding box to select it. Once selected, you can change a bounding box&apos;s size with the
-									circular white handles that appear on its corners and on each side. Or use the class editor to change the box&apos;s label.
+									<b>Single-click</b> an existing bounding box to select it. Once selected, you can change a bounding boxs size with the circular
+									white handles that appear on its corners and on each side. Or use the class editor to change the boxs label.
 								</li>
 								<li>
 									<b>Drag a box</b> to move it.
@@ -262,7 +267,8 @@ export default function Docs() {
 								</li>
 								<li>
 									<b>Class List</b> of the existing classes in the dataset (filtered by the text field and with the active option highlighted in
-									purple) and, sometimes, a &quote;Create class&quote; option if the text you typed does not match an existing class.
+									purple) and, sometimes, a <span className="bg-gray-100  font-medium rounded text-sm p-1">&quote;Create class&quote;</span> option if
+									the text you typed does not match an existing class.
 								</li>
 							</ul>
 						</div>
