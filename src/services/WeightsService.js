2b29287672fa74caa09c88e65d79cf101a6a0327
kerrlabajo <labajokerr5@gmail.com>
2023-10-25 21:24:11 +0800

    Add lacking routes from backend in WeightsService

--- src/services/WeightsService.js
+++ src/services/WeightsService.js
@@ -5,12 +5,12 @@ const BASE_URL = `${config.API_URL}/weights`
 
 const WeightsService = {
 	create: (token, weight) =>
-		axios.post(`${BASE_URL}/`, weight, {
+		axios.post(`${BASE_URL}/deploy`, weight, {
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
 		}),
-	delete: (id) => axios.delete(`${BASE_URL}/${id}`),
+	delete: (id) => axios.delete(`${BASE_URL}/${id}/delete`),
 	getAll: () => axios.get(`${BASE_URL}/`),
 	getById: (id) => axios.get(`${BASE_URL}/${id}`, id),
 }
