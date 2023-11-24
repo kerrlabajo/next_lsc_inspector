4c5fef56c513acc89b5324ee943c8480139e5257
kerrlabajo <labajokerr5@gmail.com>
2023-11-24 16:46:39 +0800

    Set production url to the public url of backend's aws deployment

--- src/services/config.js
+++ src/services/config.js
@@ -6,7 +6,7 @@ if (isLocal) {
 	apiUrl = 'http://localhost:5000/api/v1'
 } else {
 	// Production
-	apiUrl = 'https://lsc-inspector-908ed02924c7.herokuapp.com/api/v1'
+	apiUrl = 'http://3.25.149.97:5000/api/v1'
 }
 
 const config = {
